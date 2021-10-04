a7c79daf858c822748183e74b3d33d5489a6824d
Semih Chelik <schelik@uwaterloo.ca>
2021-10-03 23:01:44 -0400

    latest version

---
+++ src/components/ReadOnlyRow.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default function ReadOnlyRow({ contact, handleEditClick, handleDeleteClick  }) {
+    return (
+        <tr>
+            <td>{contact.fullName}</td>
+            <td>{contact.address}</td>
+            <td>{contact.phoneNumber}</td>
+            <td>{contact.email}</td>
+            <td>
+                <button type="button" onClick={event => handleEditClick(event, contact)} >
+                    Edit
+                </button>
+                <button type="button" onClick={() => handleDeleteClick(contact.id)}>
+                    Delete
+                </button>
+            </td>
+        </tr>
+    )
+}
